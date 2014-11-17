c5b7c16178606b49d944626e7d8fd04e6e33da95
stacey-cowan <cowan.stacey.sc@gmail.com>
2014-11-17 13:54:03 -0500

    Added Mobile
    Added the JS, HTML and CSS for the mobile portfolio

--- js/main.js
+++ js/main.js
@@ -8,4 +8,143 @@ $win.on('scroll', function () {
 });
 
 
+//Portfolio Mobile
 
+var desc1 = $('.desc1');
+
+var portfolioPiece1 = $('.portfolio-piece1');
+
+var portfolioPiece1ClickHandler = function() {
+    
+    var isActive = desc1.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc1.attr('data-state', 'inactive');
+    } else {
+        desc1.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece1.on('click', portfolioPiece1ClickHandler);
+
+
+var desc2 = $('.desc2');
+
+var portfolioPiece2 = $('.portfolio-piece2');
+
+var portfolioPiece2ClickHandler = function() {
+    
+    var isActive = desc2.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc2.attr('data-state', 'inactive');
+    } else {
+        desc2.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece2.on('click', portfolioPiece2ClickHandler);
+
+
+var desc3 = $('.desc3');
+
+var portfolioPiece3 = $('.portfolio-piece3');
+
+var portfolioPiece3ClickHandler = function() {
+    
+    var isActive = desc3.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc3.attr('data-state', 'inactive');
+    } else {
+        desc3.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece3.on('click', portfolioPiece3ClickHandler);
+
+
+var desc4 = $('.desc4');
+
+var portfolioPiece4 = $('.portfolio-piece4');
+
+var portfolioPiece4ClickHandler = function() {
+    
+    var isActive = desc4.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc4.attr('data-state', 'inactive');
+    } else {
+        desc4.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece4.on('click', portfolioPiece4ClickHandler);
+
+
+var desc5 = $('.desc5');
+
+var portfolioPiece5 = $('.portfolio-piece5');
+
+var portfolioPiece5ClickHandler = function() {
+    
+    var isActive = desc5.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc5.attr('data-state', 'inactive');
+    } else {
+        desc5.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece5.on('click', portfolioPiece5ClickHandler);
+
+
+var desc6 = $('.desc6');
+
+var portfolioPiece6 = $('.portfolio-piece6');
+
+var portfolioPiece6ClickHandler = function() {
+    
+    var isActive = desc6.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc6.attr('data-state', 'inactive');
+    } else {
+        desc6.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece6.on('click', portfolioPiece6ClickHandler);
+
+
+var desc7 = $('.desc7');
+
+var portfolioPiece7 = $('.portfolio-piece7');
+
+var portfolioPiece7ClickHandler = function() {
+    
+    var isActive = desc7.attr('data-state');
+    
+    if (isActive == 'active') {
+        desc7.attr('data-state', 'inactive');
+    } else {
+        desc7.attr('data-state', 'active');    
+    }
+    
+    
+};
+
+portfolioPiece7.on('click', portfolioPiece7ClickHandler);
