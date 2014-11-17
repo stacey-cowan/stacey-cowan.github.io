a5bc1664eacc0075ffd52b239e2b7b729ae7d113
stacey-cowan <cowan.stacey.sc@gmail.com>
2014-11-17 15:04:23 -0500

    Attempt at Fading
    Trying to fade the header during scroll; did not work; not necessary.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-var $win = $(window);
-var $introSection = $('.intro-section')
-
-$win.on('scroll', function () {
-    var scrollPos = $win.scrollTop();
-    
-    $introSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
-});
-
 
 //Portfolio Mobile
 
