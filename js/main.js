c95ed7e035c24932e971e4b190142d9a7aac9d79
stacey-cowan <cowan.stacey.sc@gmail.com>
2016-02-02 12:47:04 -0500

    Home page
    Finished the homepage updates, added transitions. Started portfolio
    page.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
-            $(function() {
+/*
+          $(function() {
                 $('ul.nav a').bind('click',function(event){
                     var $anchor = $(this);
                     
@@ -11,7 +11,28 @@
                     $('html, body').stop().animate({
                         scrollTop: $($anchor.attr('href')).offset().top
                     }, 1000);
-                    */
+                    *//*
                     event.preventDefault();
                 });
             });
+*/
+
+
+;(function ($) {
+  'use strict';
+  var content  = $('#main').smoothState({
+        // onStart runs as soon as link has been activated
+        onStart : {
+          
+          // Set the duration of our animation
+          duration: 350,
+          
+          // Alterations to the page
+          render: function () {
+
+            // Quickly toggles a class and restarts css animations
+            content.toggleAnimationClass('is-exiting');
+          }
+        }
+      }).data('smoothState'); // makes public methods available
+})(jQuery);
